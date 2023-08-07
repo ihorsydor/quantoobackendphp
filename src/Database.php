2916891fe5115400ea32ac8c981ebfd14bd5162d
ihor <ihor@poczta.onet.pl>
2023-08-07 08:18:09 +0200

    database create if not exist

--- src/Database.php
+++ src/Database.php
@@ -7,21 +7,30 @@ class Database
                                 private string $user,
                                 private string $password)
     {}
-        
+
     public function getConnection(): PDO
     {
         $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
-        
-        return new PDO($dsn, $this->user, $this->password, [
+
+        $connection = new PDO($dsn, $this->user, $this->password, [
             PDO::ATTR_EMULATE_PREPARES => false,
             PDO::ATTR_STRINGIFY_FETCHES => false
         ]);
 
+      
+        $this->checkAndCreateTables($connection);
+
+        return $connection;
+    }
+
+    private function checkAndCreateTables(PDO $connection)
+    {
         $this->createAuthorTableIfNotExists($connection);
         $this->createBookTableIfNotExists($connection);
 
-        return $connection;
-
+        
+        $this->checkAndCreateAuthorColumns($connection);
+        $this->checkAndCreateBookColumns($connection);
     }
 
     private function createAuthorTableIfNotExists(PDO $connection)
@@ -54,4 +63,89 @@ class Database
         $connection->exec($sql);
     }
 
+    private function checkAndCreateAuthorColumns(PDO $connection)
+    {
+        
+        $expectedColumns = [
+            'id',
+            'name',
+            'country'
+        ];
+
+        $sql = "SHOW COLUMNS FROM author";
+        $stmt = $connection->query($sql);
+        $existingColumns = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+      
+        $missingColumns = array_diff($expectedColumns, $existingColumns);
+
+        if (!empty($missingColumns)) {
+            foreach ($missingColumns as $column) {
+                $this->createAuthorColumn($connection, $column);
+            }
+        }
+    }
+
+    private function createAuthorColumn(PDO $connection, string $columnName)
+    {
+      
+        $columnDefinition = match ($columnName) {
+            'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
+            'name' => 'VARCHAR(255) NOT NULL',
+            'country' => 'VARCHAR(255) NOT NULL',
+        };
+
+        $sql = "ALTER TABLE author ADD $columnName $columnDefinition";
+        $connection->exec($sql);
+    }
+
+    private function checkAndCreateBookColumns(PDO $connection)
+    {
+       
+        $expectedColumns = [
+            'id',
+            'name',
+            'publishing',
+            'siteNumber',
+            'author',
+            'file',
+            'imageName',
+            'newFileName',
+            'destination',
+            'imagePath',
+        ];
+
+        $sql = "SHOW COLUMNS FROM book";
+        $stmt = $connection->query($sql);
+        $existingColumns = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+        
+        $missingColumns = array_diff($expectedColumns, $existingColumns);
+
+        if (!empty($missingColumns)) {
+            foreach ($missingColumns as $column) {
+                $this->createBookColumn($connection, $column);
+            }
+        }
+    }
+
+    private function createBookColumn(PDO $connection, string $columnName)
+    {
+       
+        $columnDefinition = match ($columnName) {
+            'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
+            'name' => 'VARCHAR(255) NOT NULL',
+            'publishing' => 'VARCHAR(255) NOT NULL',
+            'siteNumber' => 'INT NOT NULL',
+            'author' => 'INT NOT NULL',
+            'file' => 'VARCHAR(255) NOT NULL',
+            'imageName' => 'VARCHAR(255) NOT NULL',
+            'newFileName' => 'VARCHAR(255) NOT NULL',
+            'destination' => 'VARCHAR(255) NOT NULL',
+            'imagePath' => 'VARCHAR(255) NOT NULL',
+        };
+
+        $sql = "ALTER TABLE book ADD $columnName $columnDefinition";
+        $connection->exec($sql);
+    }
 }
